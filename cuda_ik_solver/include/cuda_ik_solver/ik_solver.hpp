8f0602ad888799f1d3be57699dd72a25518f6168
Vinothhk <iamvinothkumar16@gmail.com>
2025-07-01 19:17:33 +0530

    new deps

--- cuda_ik_solver/include/cuda_ik_solver/ik_solver.hpp
+++ cuda_ik_solver/include/cuda_ik_solver/ik_solver.hpp
@@ -1,20 +1,24 @@
-// #pragma once
+#ifndef CUDA_IK_SOLVER_HPP
+#define CUDA_IK_SOLVER_HPP
 
-// #include <vector>
-// #include <geometry_msgs/msg/pose.hpp>
-
-// namespace cuda_ik_solver {
-
-// std::vector<double> solveIKCUDA(const geometry_msgs::msg::Pose& target_pose);
-
-// }
-// ik_solver.hpp
-
-#pragma once
-#include <geometry_msgs/msg/pose.hpp>
 #include <vector>
+#include <geometry_msgs/msg/pose.hpp>
 
 namespace cuda_ik_solver {
+
   std::vector<double> solveIKCUDA(const geometry_msgs::msg::Pose& target_pose,
                                    const std::vector<double>& current_joint_angles);
+  void forwardKinematics(const std::vector<double>& joint_angles, float* ee_out);
+
 }
+
+#endif
+
+// #pragma once
+// #include <geometry_msgs/msg/pose.hpp>
+// #include <vector>
+
+// namespace cuda_ik_solver {
+//   std::vector<double> solveIKCUDA(const geometry_msgs::msg::Pose& target_pose,
+//                                    const std::vector<double>& current_joint_angles);
+// }
